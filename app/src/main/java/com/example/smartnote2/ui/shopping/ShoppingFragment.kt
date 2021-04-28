c1b85782f155a5f084f3362f0d46ff992478cdc2
natlight <novykovanatalie@gmail.com>
2021-04-28 22:05:49 +0200

    Add delete button to the ShoppingItem.kt Add EditText quantity to the ShoppingItem.kt.

--- app/src/main/java/com/example/smartnote2/ui/shopping/ShoppingFragment.kt
+++ app/src/main/java/com/example/smartnote2/ui/shopping/ShoppingFragment.kt
@@ -1,6 +1,7 @@
 package com.example.smartnote2.ui.shopping
 
-import android.app.AlertDialog
+import CustomAdapterShopping
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
@@ -11,7 +12,11 @@ import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.smartnote2.R
+import com.example.smartnote2.ui.sales.CardSales
+import com.example.smartnote2.ui.sales.CustomAdapterSales
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.gson.Gson
@@ -21,26 +26,29 @@ import java.lang.reflect.Type
 
 class ShoppingFragment : Fragment() {
     lateinit var editText: EditText
-    lateinit var listView: ListView
-    lateinit var fab: ExtendedFloatingActionButton
-    var list: ArrayList<String> = ArrayList()
+    lateinit var recyclerViewShopping: RecyclerView
+    lateinit var addBtn: ExtendedFloatingActionButton
+    var list: ArrayList<ShoppingItem> = ArrayList()
+    lateinit var customAdapterShopping: CustomAdapterShopping
     val SP_LIST_KEY = "SP_LIST_KEY"
-    lateinit var arrayAdapter: ArrayAdapter<String>
 
 
+
+    @SuppressLint("WrongConstant")
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        list = getArrayList(SP_LIST_KEY)
-
         val root = inflater.inflate(R.layout.fragment_shopping, container, false)
-
-        listView = root.findViewById(R.id.listView)
+        recyclerViewShopping = root.findViewById(R.id.recyclerViewShopping)
+        recyclerViewShopping.layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
+        customAdapterShopping = CustomAdapterShopping(list)
+        recyclerViewShopping.adapter = customAdapterShopping
+        customAdapterShopping.notifyDataSetChanged()
         editText = root.findViewById(R.id.editText)
 
-        fab = root.findViewById(R.id.btnAdd)
+        addBtn = root.findViewById(R.id.btnAdd)
 
 
         val ttb = AnimationUtils.loadAnimation(context, R.anim.ttb)
@@ -48,25 +56,21 @@ class ShoppingFragment : Fragment() {
         val btt = AnimationUtils.loadAnimation(context, R.anim.btt)
         val btn = AnimationUtils.loadAnimation(context, R.anim.btn)
 
-        fab.startAnimation(ttb)
+        addBtn.startAnimation(ttb)
         editText.startAnimation(btt)
-        listView.startAnimation(btn)
+        recyclerViewShopping.startAnimation(btn)
 
 
-        clearElements()
-        arrayAdapter=
-            context?.let { ArrayAdapter<String>(it, android.R.layout.simple_list_item_1, list) }!!
-
-        arrayAdapter.notifyDataSetChanged()
-        listView.adapter = arrayAdapter
-
-        fab.setOnClickListener {
-            fab.startAnimation(atb)
+        addBtn.setOnClickListener {
+            addBtn.startAnimation(atb)
+            //create shopingitem
             if(editText.length() > 0) {
-            list.add(editText.text.toString())
+                var shoppingItem = ShoppingItem()
+                shoppingItem.itemname = editText.text.toString()
+                list.add(shoppingItem)
             }
             editText.setText("")
-            arrayAdapter.notifyDataSetChanged()
+            customAdapterShopping.notifyDataSetChanged()
         }
 
         return root
@@ -77,7 +81,7 @@ class ShoppingFragment : Fragment() {
         saveArrayList(list, SP_LIST_KEY)
     }
 
-    fun saveArrayList(list: ArrayList<String>, key: String?) {
+    fun saveArrayList(list: ArrayList<ShoppingItem>, key: String?) {
         val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity)
         val editor: SharedPreferences.Editor = prefs.edit()
         val gson = Gson()
@@ -86,34 +90,16 @@ class ShoppingFragment : Fragment() {
         editor.apply()
     }
 
-    fun getArrayList(key: String?): ArrayList<String> {
+    fun getArrayList(key: String?): ArrayList<ShoppingItem> {
         val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity)
         val gson = Gson()
         val json: String? = prefs.getString(key, null)
         Log.e("dnovykov", "JSON FROM sp: " + json)
-        val type: Type = object : TypeToken<ArrayList<String?>?>() {}.getType()
+        val type: Type = object : TypeToken<ArrayList<ShoppingItem?>?>() {}.getType()
         if (json != null) {
             return gson.fromJson(json, type)
         }
         return ArrayList()
     }
 
-    fun clearElements() {
-        listView.setOnItemClickListener { parent, view, position, id ->
-            val builder = AlertDialog.Builder(context)
-
-            builder.setMessage("Delete the god?")
-
-            builder.setPositiveButton("YES"){dialog, which ->
-                list.removeAt(position)
-                 arrayAdapter.notifyDataSetChanged()
-            }
-            builder.setNegativeButton("No"){dialog,which ->
-
-            }
-            val dialog: AlertDialog = builder.create()
-
-            dialog.show()
-             }
-        }
 }
---
+++ app/src/main/java/com/example/smartnote2/ui/shopping/ShoppingItem.kt
@@ -0,0 +1,6 @@
+package com.example.smartnote2.ui.shopping
+
+class ShoppingItem {
+    lateinit var itemname: String
+    var quantity: Int = 1
+}
