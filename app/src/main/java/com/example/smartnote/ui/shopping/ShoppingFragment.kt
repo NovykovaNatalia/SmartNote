4fd375e88c0d902df180637af78646406caf70ac
natlight <novykovanatalie@gmail.com>
2021-10-07 16:28:32 +0200

    Add STT features to ShoppingFragment.kt

--- app/src/main/java/com/example/smartnote/ui/shopping/ShoppingFragment.kt
+++ app/src/main/java/com/example/smartnote/ui/shopping/ShoppingFragment.kt
@@ -2,9 +2,12 @@ package com.example.smartnote.ui.shopping
 
 import CustomAdapterShopping
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.speech.RecognizerIntent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +24,8 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class ShoppingFragment : Fragment() {
@@ -29,6 +34,8 @@ class ShoppingFragment : Fragment() {
     lateinit var addBtn: Button
     lateinit var list: ArrayList<ShoppingItem>
     lateinit var customAdapterShopping: CustomAdapterShopping
+    lateinit var btnSpeach: ImageView
+    private  val REQUEST_CODE_STT = 1
 
     @SuppressLint("WrongConstant")
     override fun onCreateView(
@@ -45,6 +52,7 @@ class ShoppingFragment : Fragment() {
         recyclerViewShopping.adapter = customAdapterShopping
         customAdapterShopping.notifyDataSetChanged()
         editText = root.findViewById(R.id.editText)
+        btnSpeach = root.findViewById(R.id.btn_speach)
 
         addBtn = root.findViewById(R.id.btnAdd)
 
@@ -54,10 +62,13 @@ class ShoppingFragment : Fragment() {
         val btt = AnimationUtils.loadAnimation(context, R.anim.btt)
         val btn = AnimationUtils.loadAnimation(context, R.anim.btn)
 
+        btnSpeach.startAnimation(ttb)
         addBtn.startAnimation(ttb)
         editText.startAnimation(ttb)
         recyclerViewShopping.startAnimation(ttb)
-
+        btnSpeach.setOnClickListener{
+            speak()
+        }
 
         addBtn.setOnClickListener {
             addBtn.startAnimation(atb)
@@ -72,6 +83,45 @@ class ShoppingFragment : Fragment() {
         }
         return root
     }
+
+    private fun speak() {
+        val mIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        mIntent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi, speak something")
+
+        try {
+            startActivityForResult(mIntent, REQUEST_CODE_STT)
+            Log.e("natl", "input")
+        } catch (e: Exception) {
+            Toast.makeText(activity, e.message, Toast.LENGTH_LONG).show()
+            Log.e("natl", "exception")
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            // Handle the result for our request code.
+            REQUEST_CODE_STT -> {
+                // Safety checks to ensure data is available.
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    // Retrieve the result array.
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    // Ensure result array is not null or empty to avoid errors.
+                    if (!result.isNullOrEmpty()) {
+                        // Recognized text is in the first position.
+                        val recognizedText = result[0]
+                        // Do what you want with the recognized text.
+                        editText.setText(recognizedText)
+                    }
+                }
+            }
+        }
+    }
+
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
