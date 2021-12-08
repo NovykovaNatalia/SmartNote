c92d59ac68d29bd8084df2467d9597c38ce06b3c
natlight <novykovanatalie@gmail.com>
2021-12-08 23:47:12 +0100

    Customize date in Event.kt

--- app/src/main/java/com/example/smartnote/ui/events/Event.kt
+++ app/src/main/java/com/example/smartnote/ui/events/Event.kt
@@ -1,6 +1,7 @@
 package com.example.smartnote.ui.events
 
 import java.io.Serializable
+import java.text.SimpleDateFormat
 import java.util.*
 
 class Event : Serializable {
@@ -9,8 +10,11 @@ class Event : Serializable {
     lateinit var time: String
 
     override fun toString(): String {
+        val date = Date(date_ev)
+        var df = SimpleDateFormat("dd/MM/yy")
+        var dateText = df.format(date)
         val builder = StringBuilder()
-        builder.append("Date" + " : " + date_ev + "\n")
+        builder.append("Date" + " : " + dateText + "\n")
                 .append("Time" + " : " + time + "\n")
                 .append("Event" + " : " + event + "\n" + "\n")
         return builder.toString()
