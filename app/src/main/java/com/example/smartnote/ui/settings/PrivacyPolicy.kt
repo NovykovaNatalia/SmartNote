f666e8b0051d9e12cd46e48a7fab56aa06d9e730
natlight <novykovanatalie@gmail.com>
2021-12-08 23:35:39 +0100

    Customize PrivacyPolicy.kt, rate_dialog.xml, strings.xml

--- app/src/main/java/com/example/smartnote/ui/settings/PrivacyPolicy.kt
+++ app/src/main/java/com/example/smartnote/ui/settings/PrivacyPolicy.kt
@@ -19,27 +19,24 @@ class PrivacyPolicy : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
 
         val users = ArrayList<User>()
-        users.add(User("\n" + "Privacy Policy", "Last updated: October 10, 2021\n" +
+        users.add(User("\n" + "Privacy Policy", "Last updated: December 8, 2021\n" +
                 "\n" +
-                "User privacy is the top priority. We (“3ND Tech Co, \"We\", \"Us\", or \"Our\") value your (\"User\", \"You\" or \"Your\") " +
-                "privacy and recognize the sensitivity of your personal information. We are committed to protecting your personal information " +
-                "and using it only when appropriate to provide you with the best possible services and products.\n" +
+                "User privacy is the top priority. We (“3&D”) value your (“User”, “You” or “Your”) privacy and recognize the sensitivity of your personal information. We are committed " +
+                "to protecting your personal information and using it only when appropriate to provide you with the best possible services and products.\n\n" +
                 "\n" +
                 "BY ACCESSING OR USING OUR SERVICES, YOU AGREE TO THE COLLECTION AND USE OF INFORMATION IN ACCORDANCE WITH THIS POLICY." +
                 " THIS PRIVACY POLICY IS PART OF AND INCORPORATED INTO OUR TERMS OF USE(\"Terms of Use\"). IF YOU DO NOT AGREE TO ALL OF THE TERMS " +
                 "AND CONDITIONS SET FORTH BELOW, YOU MAY NOT USE OUR SERVICES\n" +
                 "\n" +
-                "Should you have any questions related to this policy or our practices around privacy and data protection in general, " +
-                "please don’t hesitate to contact us.\n"))
-        users.add(User("\n" + "Definitions\n", "Personal Information means information about a living individual who can be " +
-                "identified from the information, including your name, address, email, telephone number, fax number, geographic location, " +
-                "etc.\n" +
-                "\n" +
-                "Non-Personal Information means information that cannot be used to identify a specific individual. For example, " +
-                "the duration of a page visit, the time when you use a service."
+                "Should you have any questions related to this policy or our practices around privacy and data protection in general, please don’t " +
+                "hesitate to contact us as indicated in the contact section of this policy.\n"))
+        users.add(User("\n" + "Definitions\n", "Personal Information means information about a living individual who can be identified " +
+                "from the information, including your name, address, email, telephone number, fax number, geographic location, etc.\n" +
+                "Non-Personal Information means information that cannot be used to identify a specific individual. For example, the duration of a " +
+                "page visit, the time when you use a service.\n"
                 +
                 "\n"))
-        users.add(User("\n" + "What Information We Collect and How It Is Used?\n", " Generally, we do not collect " +
+        users.add(User("\n" + "What Information We Collect and How It Is Used?\n", "Generally, we do not collect " +
                 "any Personal Information when you use our applications or websites. We do not require users to register before using " +
                 "our service, nor do we require users to provide us with any Personal Information.\n" +
                 "\n" +
@@ -71,40 +68,32 @@ class PrivacyPolicy : AppCompatActivity() {
                 "accessing the site, to personalize your experience, and to remember your preferences. If your browser or device is set" +
                 "not to accept cookies or if you reject a cookie, you may not be able to access certain features or services." +
                 "\n"))
-        users.add(User("\n" + "How Long We Retain Your Information?\n", " We do not collect or store your Personal Information. If you voluntarily send us an email," +
-                " we will keep your email address" +
-                " and related information for a necessary and relevant time, so that we can contact you to give you feedback or improve our services." +
-                "\n" +
-                "\n" + "We will retain some of the Non-Personal Information for internal analysis purposes. Non-Personal Information is generally " +
-                "retained for a shorter period, except when this data is used to strengthen the security or to improve the functionality of our " +
-                "services, or we are legally obligated to retain this data for longer time periods." +
-                "\n" +
-                "We may provide paid products and/or services in our products. In this case, We utilize Apple and Google to process all financial transactions.\n" +
-                "Apple Store In-App Payments: You can check their Privacy Policy at:\n" +
-                "https://www.apple.com/legal/privacy/\n" +
-                "\n" +
-                "Google Play In-App Payments: You can check their Privacy Policy at:\n" +
-                "https://policies.google.com/privacy\n" +
-                "\n" +
-                "We may use third-party service providers to show you advertisements to help support and maintain our services.\n" +
-                "Admob from Google: You can check their Privacy Policy at:https://policies.google.com/privacy\n" +
-                "\n" +
-                "Mopub from Twitter: You can check their Privacy Policy at:https://www.mopub.com/en/legal/privacy\n" +
-                "\n" +
-                " Facebook Audience Network: You can check their Privacy Policy at:https://www.facebook.com/about/privacy/\n" +
-                "\n" +
-                "We may use third-party service providers to monitor and analyze the usage of our services.Facebook Analytics: You can check their Privacy Policy at:" +
-                "https://www.facebook.com/about/privacy/\n" +
-                "Firebase and Google Analytics: You can check their Privacy Policy at:\n" +
-                "https://policies.google.com/privacy\n" +
-                "We provide optional functionalities which allow you to sync your contents to your iCloud or Google Drive.\n" +
-                "i Cloud from Apple: You can check their Privacy Policy at:\n" +
-                "https://www.apple.com/legal/internet-services/icloud/\n" +
-                "Google Drive: You can check their Privacy Policy at:https://policies.google.com/privacy\n"))
+        users.add(User("\n" + "How Long We Retain Your Information?\n", " We do not collect or store your Personal Information. If you voluntarily" +
+                " send us an email, we will keep your email address" +
+                " and related information for a necessary and relevant time, so that we can contact you to give you feedback or improve our services.\n" +
+                "We will retain some of the Non-Personal Information for internal analysis purposes. Non-Personal Information is generally retained for a shorter period, " +
+                "except when this data is used to strengthen the security or to improve the functionality of our services, or we are legally obligated to retain this data for longer time periods.\n" +
+                "\n"))
         users.add(User("\n" + "Security\n", "We value your trust in providing us your information, thus we are striving to use " +
                 "commercially acceptable means of protecting it. But remember that no method of transmission over the internet," +
                 " or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.\n"))
-
+        users.add(User("\n" + "Your Rights\n", "Decide whether to use our service: You can decide whether to download our applications. " +
+                "When we update our policies," +
+                " you can decide whether to continue using our services or not. You can opt-out of certain functions in our application settings." +
+                "Decide on the permissions granted to the service: You can decide whether to grant certain permissions to our services in the system settings. " +
+                "Please note that some functions cannot be run without specific permissions.If you are a resident of the European Union (EU) and the European Economic Area (EEA), you have " +
+                "certain data protection rights described in the General Data Protection Regulation (GDPR).\n" +
+                "If you are a California resident, you can implement your rights as provided in the California Consumer Privacy Act (CCPA)\n" +
+                "If you have any requirements or questions, please contact us at: natlight.todo@gmail.com.\n"))
+        users.add(User("\n" + "Children’s Privacy\n","Our Services are not intended for use by children under the age of 13 (“Children”). We are committed to protecting " +
+                "the privacy needs of children and we encourage parents and guardians to take an active role in their children’s online activities and interests. We do not knowingly collect information " +
+                "from children under the age of 13, and in the event that we learn or receive " +
+                "a valid notice that a child under the age of 13 has provided information on the service, we will delete that information as soon as possible.\n" ))
+        users.add(User("\n" + "Changes to This Privacy Policy\n", " \n" +
+                "We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. " +
+                "We will notify you of any changes by posting the new Privacy Policy on this page.\n"))
+        users.add(User("\n" + "Contact Us\n", "If you have any questions or suggestions about our Privacy Policy, please do not hesitate to contact " +
+                "us at:natlight.todo@gmail.com\n"))
         val adapter = PrivPolicyAdapter(users)
         recyclerView.adapter = adapter
     }
